e9d9660329c5ac7f8666b59a19fe17dbf990f32e
chaimaeboultam <chaimaeboultam2002@gmail.com>
2023-12-23 09:32:27 -0800

    .

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursive(int *array, int low, int high, size_t size);
 size_t lomuto_partition(int *array, int low, int high, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
